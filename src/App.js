d5e708a8dc7a59954effe03f3f70bac121b1d85b
kareemmoheb <kmoheb92@gmail.com>
2022-10-26 01:53:28 +0200

    adding stepper composnent to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React, { useState } from 'react'
+import { Stepper } from './components/Stepper';
 import './App.css';
 
 function App() {
+    const totalSteps = 3;
+    const [currentStep, setCurrentStep] = useState(1);
     return (
-        <div className="App">
+        <div className="App container">
+            <Stepper totalSteps={totalSteps} currentStep={currentStep} setCurrentStep={setCurrentStep} />
         </div>
     );
 }
